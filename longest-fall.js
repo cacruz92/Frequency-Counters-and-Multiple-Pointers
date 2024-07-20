706a5ed8c3248a4e09ae3ef088c44b8b967604b6
Christian Cruz <christian.cruz92@hotmail.com>
2024-07-20 16:56:31 -0400

    added remaing functions

--- longest-fall.js
+++ longest-fall.js
@@ -1,3 +1,21 @@
 // add whatever parameters you deem necessary
-function longestFall() {
+function longestFall(arr) {
+    let counter = 1;
+    let maxCounter = 0;
+  
+    // quick fail case if the array is empty
+    if (arr.length === 0) {
+      return 0;
+    }
+  
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < arr[i - 1]) {
+        counter++;
+        maxCounter = Math.max(counter, maxCounter);
+      } else {
+        counter = 1;
+      }
+    }
+  
+    return maxCounter || 1;
 }
